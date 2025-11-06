dbb952ad51673d593ae4828fedf370c17817c5a8
Milan-Treh4n <trehanmilan5@gmail.com>
2025-11-06 15:47:41 +0000

    Adding a play again option

--- Rock Paper Scissors game/Code.py
+++ Rock Paper Scissors game/Code.py
@@ -7,6 +7,12 @@ if not user_choice:
     print("No choice made! Exiting the game.")
     exit()
 
+# Ask user if they would like to continue playing
+play_again = input("Do you want to play the game? (yes/no): ").lower()
+if play_again != 'yes':
+    print("Thanks for playing! Goodbye.")
+    exit()
+
 # Validate user input
 if user_choice not in ['rock', 'paper', 'scissors']:
     print("Invalid choice! Please choose rock, paper, or scissors.")
