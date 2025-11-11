f30838d6c5b121db852332d274f83502e364e011
Milan-Treh4n <trehanmilan5@gmail.com>
2025-11-11 13:28:32 +0000

    Updated file with base64 function to display images

--- Rock Paper Scissors game/rock_paper_scissors.py
+++ Rock Paper Scissors game/rock_paper_scissors.py
@@ -1,24 +1,29 @@
+import os
+import json
+import random
 import base64
 import tempfile
+from io import BytesIO
 from PIL import Image
 
-rock_win_b64 = """/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxMTEhUSEhIVFRUXFxcXFxcYFxcVGBcXFRcXFxcXFRcYHSggGBolGxcXITEhJSkrLi4uFx8zODMtNygtLisBCgoKDg0OGBAQGi0dICUtLS8tKy03Ly0tKy0uLS0tLS0tLS0tKy0tLS8tLS0tKy0rNistLSstKy0rKy0tLy0tK//AABEIAMABBgMBIgACEQEDEQH/xAAbAAACAwEBAQAAAAAAAAAAAAABAgADBQQGB//EAEEQAAEDAgIFCAkCBQMFAQAAAAEAAhEDIQQxBRJBUWEGIlNxkZKh0hMWMoGxwdHh8BRC"""
-paper_win_b64 = """/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxAPDw8NDw8NDw8PDQ4PDw4NDw8NDw4OFREWFxURFRYYHSggGBolHRUVLTIhJykrLi4uFx8zODMsNygtLisBCgoKDg0NFQ8PFSsZFRkrKysrKysrKy0rKy0rKzArKysrLS03Kys3LTcrKy0rKy0tLTIrKysrNzc3LS0rKystLf/AABEIALcBEwMBIgACEQEDEQH/xAAbAAACAwEBAQAAAAAAAAAAAAAAAQ"""
-scissors_win_b64 = """/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBw8PDw8QDxAPDw8NDw0QDxAPDw8PEA8PFREWFhURFRUYHSggGBolGxUVITEhJSkrLi4uFx8zODMtNygtLisBCgoKDg0OFxAQFy8lIB8tLSstLS0tLSsrKy0tLS0tLS0tNSstLS8uLS0tLS0rLSstLy0rLS0tLS0vLS0rLS0tK//AABEIALcBEwMBEQACEQEDEQH/xAAbAAEBAAMBAQEAAAAAAAAAAAABAAIEBgUDB//EAEcQAAIBAgIGBgQJCgUFAAAAAAABAgMRBAUGEiExUXEiQWGBkaETMnKxI0JSU2KSssHRFBYkM3OCk6Kz4TRDg9LwFVRjwsP/xAAaAQEBAQEBAQEAAAAAAAAAAAAAAQIDBQQG/8QAMREBAAIBAgMECAcBAQAAAAAAAAECAwQRBSExEkFRkRMyUnGBobHRFBUiQmHB4fBD/9oADAMBAAIRAxEAPwD9VUCjJRAyUS7IyURsHVAySIMki7BQ2GVgFIBQCBARBFEBECBAAHDaZ/4+i/8Aw0/6lQkulejpcufQXIrDbAGQQEwCwAwICIMQIDFoAaKMANhRNDJIIysApANibhsAhSkVGRBDcQCBAQEBAIEFAEBw2mv+Nofsaf8AUqEluvR0mWPoLkVhuEAwICAAC4EyAQEAMAYGLKMbAbVjQySCFIikIQIKQEBAgIIQIBAgIKgICAAOG01f6bR7KNP+pMzLdejosqfQXJFZlvMIAICALEFYACoAYAwgAxZQWA20jQUEJAgIFYKUAgQEEIEBAIEBAQVEEAAcBplO+OX0adFe+X3okulejpcol0VyKxL0QiYAQQEwCwVWAABgAAwBlRjcDcNBASIkAoBQUgQEEQUhEAgRBAQDYKrBABAfm2k1TWx1Z/JnTj9WEU/NEl2r0dTkz6MeQhzl6xUFiCAgIACgCAAIAAxZRBG2aCiSiAQFAQUgQCESAgEggICAQECAxA+darGEZTm1GME3KT2JRW9lH5XiK/pK06nzlSU+WtJu3mc5d9tna5M+ijTnL2UVlEAAhQBjJ22vYlvb2Jd5JmI5yREzO0NCvnWGhvqxfsXn9m58d+IaanW8fDn9H24+Ham/Sk/Hl9Xw/OPCfLl/Dn+Bz/NdL7Xyl1/KNV7MecPpTzzCy3Vor21KHvR0pxDTW6Xj48vq534bqq9aT8Np+jfhNSV4tST3OLUl4o+ytotG8TvD4rVms7WjaSVGLKADcNISSiAQJAICBBSERAgQEA2ArAIEASklvCvKxOeUoNxjGU5L2Yx73/Yz2moxy4zSDPZ4l6qdqUXsjHdKXF8ewu7cViHkUt65mWnc5N6qNQ5S9uO4MoCChgeTnGeQoXjG06vC/Rj7T49h5ut4lTB+mvO3h4e/7PS0XDb5/wBVuVfnPu+7j8Zj6lZ"""
-computer_win_b64 = """PASTE COMPUTER BASE64 HERE"""
+# Load base64 images from JSON
+json_path = os.path.join(os.path.dirname(__file__), "Base 64 for images", "base64_variables.json")
 
+with open(json_path, "r") as f:
+    images = json.load(f)
 
-def show_image(filename):
+# Function to display image from base64
+def show_image_from_base64(b64_string):
     try:
-        img_path = os.path.join(os.path.dirname(__file__), filename)
-        img = Image.open(img_path)
+        image_data = base64.b64decode(b64_string)
+        img = Image.open(BytesIO(image_data))
 
-        # Display image in a Python window (works on any computer)
-        plt.imshow(img)
-        plt.axis('off')
-        plt.show()
-    except Exception:
-        pass  # Silent fail if image missing
+        # Use temporary file to allow OS viewer to open it
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as tmp:
+            img.save(tmp.name, format="PNG")
+            img.show()
+    except Exception as e:
+        print("Image failed to display:", e)
 
 rounds_played = 0
 
@@ -49,20 +54,26 @@ while True:  # Keep playing until user confirms exit
     # Determine winner
     if user_choice == computer_choice:
         print("It's a tie!")
+
     elif user_choice == "rock" and computer_choice == "scissors":
         print("You win!")
-        show_image("rock_win.png")
+        show_image_from_base64(images["rock_win_b64"])
+
     elif user_choice == "paper" and computer_choice == "rock":
         print("You win!")
-        show_image("paper_win.png")
+        show_image_from_base64(images["paper_win_b64"])
+
     elif user_choice == "scissors" and computer_choice == "paper":
         print("You win!")
-        show_image("scissors_win.png")
+        show_image_from_base64(images["scissors_win_b64"])
+
     else:
         print("Computer wins!")
-        show_image("computer_win.png")
+        show_image_from_base64(images["computer_win_b64"])
 
     print("Let's play again.")
+
+
 
 
 
