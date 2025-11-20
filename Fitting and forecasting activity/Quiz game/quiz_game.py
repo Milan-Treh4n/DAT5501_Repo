cb3857a47e9c1214847aa341f7ddac22ca55f410
Milan-Treh4n <trehanmilan5@gmail.com>
2025-11-20 14:31:51 +0000

    creating quiz themes

--- Fitting and forecasting activity/Quiz game/quiz_game.py
+++ Fitting and forecasting activity/Quiz game/quiz_game.py
@@ -2,8 +2,16 @@ import random
 import string
 import hashlib
 
-# Ask the user wahat difficulty level they want
+# Ask the user what difficulty level they want
 difficulty = input("Choose a difficulty level (easy, medium, hard, challenge): ").strip().lower()
+
+# Ask the user what quiz theme they want
+theme = input("Choose a quiz theme (sport,science, history, general): ").strip().lower()
+
+# Validate the theme
+if theme not in ["sport", "science", "history", "general"]:
+    print("Invalid theme. Please choose from sport, science, history, or general.")
+    exit(1)
 
 # Set parameters based on difficulty level
 if difficulty == "easy":
